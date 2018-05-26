3ecde5eb8225b306661621b3210f1efd1bc47366
mgh3326 <mgh3326@naver.com>
2018-05-26 17:50:14 +0900

    net info update

--- src/com/edu/test/SecondServlet.java
+++ src/com/edu/test/SecondServlet.java
@@ -12,7 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/second")
 public class SecondServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         System.out.println("SecondServlet!!");
+        PrintWriter out = resp.getWriter();
+        out.print("<html><head><title>Test</title></head>");
+        out.print("<body><h1>have a nice day!!</h1></body>");
+        out.print("</html>");
+        out.close();
     }
 }
